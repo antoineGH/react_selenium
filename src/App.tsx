c5ea3c07a27ab61f0e982490a93a074136eb3e97
Antoine Ratat <antoine.ratat@gmail.com>
2021-12-24 01:43:04 +0800

    create login form

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Layout, Row, Col } from 'antd'
+import LoginForm from './components/loginForm/LoginForm'
+
 import './App.css'
 
+const { Content } = Layout
+
 function App() {
-	return <div className='App'></div>
+	return (
+		<div className='App'>
+			<Layout>
+				<Content>
+					<Row className='row-login'>
+						<Col className='col-login' span={12}>
+							<LoginForm />
+						</Col>
+					</Row>
+				</Content>
+			</Layout>
+		</div>
+	)
 }
 
 export default App
